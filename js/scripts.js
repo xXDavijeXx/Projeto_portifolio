f044b2007e085602f7842a8a20ea12c2e99340ef
xXDavijeXx <davi.djer21@gmail.com>
2022-07-01 20:17:41 -0300

    Correção de bus

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Ativa o bootstrap no elemento de navegação principal
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    // Recolher a barra de navegação responsiva quando o alternador estiver visível
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
